a346837cf736f4ba21ce9f5b4b91132aae211595
gaoxinbo <gaoxinbo1984@gmail.com>
2013-01-28 15:34:03 +0800

    add constant.h

---
+++ src/net/constant.h
@@ -0,0 +1,19 @@
+// File: constant.h
+// Author: Gao Xinbo gaoxinbo1984@gmail.com
+// Version: 1.0
+// Date: 2013-01-25
+// Copyright 2013, Gao Xinbo.  All rights reserved.
+
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+namespace gevent {
+namespace net {
+
+const int kEpollSize = 512;
+const int kListenBaklog = 512;
+
+}  // namespace net
+}  // namespace gevent
+#endif  // _CONSTANT_H_
+
